2caab4da5d8707bd284e68ce43dfbe7fc1c03732
Iván Mejía <ivanmejiapulido@gmail.com>
2018-10-23 22:33:54 +0200

    Ready to go.
    Lo hecho hasta el momento..

---
+++ ConsoleApp1/ConsoleApp1/BLL/Operaciones/Aritmetica.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraMVC.BLL.Operaciones
+{
+	class Aritmetica
+	{
+
+		public double SumarDosOperandos(double op1, double op2)
+		{
+
+			double resultado = op1 + op2;
+
+			return resultado;
+		}
+
+		public double RestarDosOperandos(double op1, double op2)
+		{
+
+			double resultado = op1 - op2;
+
+			return resultado;
+		}
+
+		public double MultiplicarDosOperandos(double op1, double op2)
+		{
+
+			double resultado = op1 * op2;
+
+			return resultado;
+		}
+
+		public double DividirDosOperandos(double op1, double op2)
+		{
+
+			//Control posible división entre 0
+			if (op2 == 0.0)
+			{
+				double warning = double.MaxValue;
+				return warning;
+
+			}
+			else
+			{
+				double resultado = op1 / op2;
+				return resultado;
+			}
+		}
+
+		public double RestoDosOperandos(double op1, double op2)
+		{
+
+			double resultado = op1 % op2;
+
+			return resultado;
+		}
+	}
+}
